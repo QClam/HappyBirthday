352ea322b1c8de3160a25b88b915b9df3880ef28
Xuan Quy <hoangxuanquy5@gmail.com>
2024-12-24 20:56:14 +0700

    Second commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const giftBox = document.querySelector('.gift-box');
 const shadow = document.querySelector('.shadow');
 const giftContainer = document.querySelector('.gift-container');
 const text = document.querySelector('.text');
+const cake = document.getElementById('cake');
+
+const audio = new Audio('cmsn.mp3');
 
 click.addEventListener('click', () => {
     if(click.className==="click") {
@@ -19,11 +22,16 @@ click.addEventListener('click', () => {
         giftContainer.classList.remove("active")
         text.classList.remove("active")
         text.classList.add("active2")
+        cake.classList.remove('active');
     }
 })
 
 document.querySelector('.gift-box').addEventListener('click', function () {
     const fireworksContainer = document.getElementById('fireworks-container');
+
+    cake.classList.add('active');
+
+    audio.play();
   
     for (let i = 0; i < 20; i++) {
       const firework = document.createElement('div');
@@ -31,7 +39,7 @@ document.querySelector('.gift-box').addEventListener('click', function () {
   
       // Random vị trí phát nổ
       const angle = Math.random() * 2 * Math.PI;
-      const distance = Math.random() * 100 + 50; // Khoảng cách phát nổ
+      const distance = Math.random() * 1000 + 50; // Khoảng cách phát nổ
       const x = Math.cos(angle) * distance + 'px';
       const y = Math.sin(angle) * distance + 'px';
   
